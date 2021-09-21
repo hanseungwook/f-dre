eda4ba2cb6a2d1ada7634d0a80667082ad63d5da
Seungwook Han <sh3264@coumbia.edu>
2021-09-21 18:43:32 -0400

    Specified loc + frame

--- src/classification/trainers/mi_classifier.py
+++ src/classification/trainers/mi_classifier.py
@@ -511,7 +511,7 @@ class MIClassifier(BaseTrainer):
         # scat2.set_offsets(np.vstack([samples, log_ratio_p_q_from_cob.cpu().detach()]).T)                    
 
         ax2.set_ylabel("Log Ratio")
-        ax2.legend(loc='best')
+        ax2.legend(loc='upper right', frameon=True)
         ax2.set_xlim([-25,25])
         ax2.set_ylim([-1000,1000])
 
